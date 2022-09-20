830371af10431f2a9cbea60f4b01af2f64ee1753
Svetlana <svdorohova@gmail.com>
2022-09-20 08:36:52 +0300

    created an array

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Введите массив из строк через запятую");
+string read = Console.ReadLine().Replace(" ", "");
+string[] userarray = read.Split(",");
