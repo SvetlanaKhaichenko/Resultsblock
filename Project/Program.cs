e7aa62a980eca45d9c6b3b831ada2bc1b25f7566
Svetlana <svdorohova@gmail.com>
2022-09-20 08:41:36 +0300

    array search method

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,4 +1,23 @@
-﻿
+﻿string[] FindArray(string[] givenmas)
+{
+    int count = 0;
+    for (int i = 0; i < givenmas.Length; i++)
+    {
+        if (givenmas[i].Length <= 3) count++;
+    }
+    string[] result = new string[givenmas.Length];
+    if (count == 0) System.Console.WriteLine("Массив не содержит строк <= 3");
+    else
+    {
+        for (int i = 0; i < givenmas.Length; i++)
+        {
+            if (givenmas[i].Length <= 3) result[i] = givenmas[i];
+        }
+    }
+    return result;
+}
+
+
 
 Console.WriteLine("Введите массив из строк через запятую");
 string read = Console.ReadLine().Replace(" ", "");
