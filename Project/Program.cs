a4ca761182488332fd3293e2447e7d21b1beb049
Svetlana <svdorohova@gmail.com>
2022-09-20 08:51:23 +0300

    array printing method

--- Project/Program.cs
+++ Project/Program.cs
@@ -17,8 +17,28 @@
     return result;
 }
 
+void PrintMas(string[] mas)
+{
+    for (int i = 0; i < 1; i++)
+    {
+        if (mas[i].Length <= 3) System.Console.Write($"[{mas[i]}, ");
+        else System.Console.WriteLine("[");
+    }
+    for (int j = 1; j < mas.Length - 1; j++)
+    {
+        if (mas[j] != null) Console.Write($"{mas[j]}, ");
+    }
+    for (int k = mas.Length - 1; k < mas.Length; k++)
+    {
+        if (mas[k].Length <= 3) System.Console.WriteLine($"{mas[k]}]");
+        else System.Console.WriteLine("]");
+    }
+    //if (mas[i] != null ) System.Console.Write($"{mas[i]} ");
+}
 
 
 Console.WriteLine("Введите массив из строк через запятую");
 string read = Console.ReadLine().Replace(" ", "");
 string[] userarray = read.Split(",");
+
+PrintMas (FindArray(userarray));
